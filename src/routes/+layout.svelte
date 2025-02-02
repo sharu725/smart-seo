33eebcb2d94d76ae99d39c2a71d6104487eb5494
sharath <sharath.dt@gmail.com>
2025-02-03 00:41:13 +0530

    add menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,7 +6,31 @@
   let { children } = $props();
 </script>
 
+<nav class="container">
+  <ul class="flex">
+    <li>
+      <a href="/">Home</a>
+    </li>
+    <li>
+      <a href="/about">About</a>
+    </li>
+    <li>
+      <a href="/blog">Blog</a>
+    </li>
+  </ul>
+</nav>
 <Seo SITE_TITLE="Smart Seo" SITE_URL="https://smart-seo.sveltethemes.dev" />
 <div class="container">
   {@render children()}
 </div>
+
+<style>
+  .flex {
+    display: flex;
+    padding: 0;
+  }
+  li {
+    margin-right: 1rem;
+    list-style: none;
+  }
+</style>

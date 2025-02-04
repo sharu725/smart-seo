5a030ecb9d5e442d5a1471e41754d24729844986
sharath <sharath.dt@gmail.com>
2025-02-04 23:02:55 +0530

    fix import

--- src/routes/blog/[blog_title]/[new]/[any]/+page.svelte
+++ src/routes/blog/[blog_title]/[new]/[any]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import getHeadTags from "$lib/get_meta_tags.js";
   import { onMount } from "svelte";
-  import Prism from "../../../Prism.svelte";
+  import Prism from "../../../../Prism.svelte";
 
   let headContent = $state("");
 
